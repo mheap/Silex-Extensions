8ca68bc73349eca91e7aae72a495d5500c069736
Sven E. <sven.eisenschmidt@gmail.com>
2011-05-19 22:41:41 -0700

    Fixed corrupt repository urls

--- vendors.sh
+++ vendors.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-git clone https://github.com/doctrine/mongodb 		      vendor/mongodb
-git clone https://github.com/nrk/predis.git 		      vendor/predis
-git clone https://github.com/fabpot/Twig.git	 	      vendor/twig
-git clone https://github.com/fate/Gravatar-php.git 	      vendor/gravatar-php
-git clone https://github.com/mandango/mandango.git        vendor/mandango
-git clone https://github.com:fate/embedly-php.git         vendor/embedly-php
-git clone https://github.com/knplabs/MarkdownBundle.git   vendor/knplabs-markdown/Knplabs/Bundle/MarkdownBundle
-git clone https://github.com/kriswallsmith/assetic.git    vendor/assetic
+git clone https://github.com/nrk/predis  		      vendor/predis
+git clone https://github.com/doctrine/mongodb 		  vendor/mongodb
+git clone https://github.com/fabpot/Twig 	 	      vendor/twig
+git clone https://github.com/mandango/mandango        vendor/mandango
+git clone https://github.com/knplabs/MarkdownBundle   vendor/knplabs-markdown/Knplabs/Bundle/MarkdownBundle
+git clone https://github.com/kriswallsmith/assetic    vendor/assetic
+git clone https://github.com/fate/Gravatar-php 	      vendor/gravatar-php
+git clone https://github.com/fate/embedly-php         vendor/embedly-php
